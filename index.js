5ec7b17f1dc95a6f34936232f2e3c20d82eda6db
Muhammad Hamza <mh408800@gmail.com>
2023-12-14 12:36:59 +0500

    setup the project

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+require("dotenv").config()
+const PORT = process.env.PORT;
+const app = express();
+
+// middlwares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// server
+app.listen(PORT, ()=>{
+    console.log(`server running at localhost:${PORT}/`)
+});
