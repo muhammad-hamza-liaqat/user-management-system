7b6b289ccba4058096df9867fd311bea3a540ddb
Muhammad Hamza <mh408800@gmail.com>
2023-12-14 15:10:32 +0500

    userModel Validations

--- Models/userModel.js
+++ Models/userModel.js
@@ -6,26 +6,90 @@ const userModel = sequelize.define("users", {
     type: DataTypes.INTEGER,
     primaryKey: true,
     allowNull: false,
+    validate: {
+      notNull: {
+        msg: "User ID is required.",
+      },
+      isInt: {
+        msg: "User ID must be an integer.",
+      },
+    },
   },
   firstName: {
     type: DataTypes.STRING,
     allowNull: false,
+    validate: {
+      notNull: {
+        msg: "First name is required.",
+      },
+      len: {
+        args: [3, 255],
+        msg: "First name must be between 3 and 255 characters.",
+      },
+    },
   },
   lastName: {
     type: DataTypes.STRING,
     allowNull: false,
+    validate: {
+      notNull: {
+        msg: "Last name is required.",
+      },
+      len: {
+        args: [3, 255],
+        msg: "Last name must be between 3 and 255 characters.",
+      },
+    },
   },
   email: {
     type: DataTypes.STRING,
     allowNull: false,
+    validate: {
+      notNull: {
+        msg: "Email is required.",
+      },
+      isEmail: {
+        msg: "Invalid email format.",
+      },
+      customValidator(value) {
+        // Custom validation for domain and "@" sign
+        if (!/@/.test(value)) {
+          throw new Error("Email must contain @ symbol.");
+        }
+
+        const allowedDomains = ["gmail.com", "yahoo.com", "hotmail.com", "icloud.com", "outlook"];
+        const domain = value.split("@")[1];
+
+        if (!allowedDomains.includes(domain)) {
+          throw new Error(
+            "Invalid email domain!"
+          );
+        }
+      },
+    },
   },
   password: {
     type: DataTypes.STRING,
     allowNull: false,
+    validate: {
+      notNull: {
+        msg: "Password is required.",
+      },
+      len: {
+        args: [6, 255],
+        msg: "Password must be between 6 and 255 characters.",
+      },
+      containsSpecialCharacter(value) {
+        // Custom validation for at least one special character
+        if (!/[!@#$%^&*(),.?":{}|<>]/.test(value)) {
+          throw new Error("Password must contain at least one special character.");
+        }
+      },
+    },
   },
   rememberToken: {
     type: DataTypes.STRING,
-    allowNull: false,
+    allowNull: true,
   },
   isAdmin: {
     type: DataTypes.BOOLEAN,
@@ -34,10 +98,6 @@ const userModel = sequelize.define("users", {
   },
   isVerified: {
     type: DataTypes.BOOLEAN,
-    allowNull: false,
-  },
-  timestamp: {
-    type: DataTypes.timestamp,
     allowNull: false,
   },
 });
