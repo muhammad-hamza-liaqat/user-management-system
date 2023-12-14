9128c6f520c1d990b945c72ef0d862876b51b4db
Muhammad Hamza <mh408800@gmail.com>
2023-12-14 18:24:40 +0500

    token expiry validation added

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -5,19 +5,24 @@ const { v4: uuidv4 } = require("uuid");
 const getUser = async (req, res) => {
   res.status(200).json({ message: "hello from add-user Controller" });
 };
+
 const createUser = async (req, res) => {
   // res.end("add user post method")
   const { firstName, lastName, email, password } = req.body;
   try {
     await userModel.sync();
     // rememberToekn for the user to verify himself against this token
-    const rememberTokenForUser = uuidv4();
+    const rememberTokenForUser = {
+      token: uuidv4(),
+      createdAt: new Date(),
+    };
     // hashing the password and generate 10 rounds of salt for password decryption
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
     const newUser = await userModel.create({
       ...req.body,
       password: hashedPassword,
-      rememberToken: rememberTokenForUser,
+      rememberToken: rememberTokenForUser.token,
       isAdmin: false,
       isVerified: false,
     });
@@ -29,6 +34,14 @@ const createUser = async (req, res) => {
   }
 };
 
+const isValidToken = (tokenObject) => {
+  const createdAt = new Date(tokenObject.createdAt);
+  const currentTime = new Date();
+  const elapsedMilliseconds = currentTime - createdAt;
+  const elapsedMinutes = elapsedMilliseconds / (1000 * 60);
+  return elapsedMinutes <= 1;
+};
+
 const verifyUserToken = async (req, res) => {
   const { email, rememberToken } = req.body;
   try {
@@ -36,11 +49,16 @@ const verifyUserToken = async (req, res) => {
     if (!user) {
       return res.status(400).json({ message: "Invalid email entered!" });
     }
-    if (user.rememberToken === rememberToken) {
-      user.rememberToken = null;
-      user.isVerified = true;
-      user.save();
-      return res.status(200).json({ message: "user verified!" });
+    if (isValidToken({ createdAt: user.createdAt })) {
+      if (user.rememberToken === rememberToken) {
+        user.rememberToken = null;
+        user.isVerified = true;
+        user.save();
+        return res.status(200).json({ message: "user verified!" });
+      }
+    }
+    else {
+        return res.status(403).json({message: "token has expired"})
     }
     return res.status(403).json({ message: "invalid token!" });
   } catch (error) {
@@ -59,14 +77,16 @@ const userLogin = async (req, res) => {
         email: email,
       },
     });
+    // user doesnot exists
     if (!user) {
       return res.status(401).json({ message: "Invalid Email or Password!" });
     }
     const validatePassword = await bcrypt.compare(password, user.password);
+    // incorrect password
     if (!validatePassword) {
       return res.status(401).json({ message: "Invalid Email or Password" });
     }
-    console.log("user login successfully!")
+    console.log("user login successfully!");
     return res.status(200).json({ message: "login successfully!" });
   } catch (error) {
     return res
