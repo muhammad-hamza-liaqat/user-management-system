eec7645234845b8075822cd36671a63957462360
Muhammad Hamza <mh408800@gmail.com>
2023-12-17 14:17:13 +0500

    express - fileUpload

--- Controllers/jobController.js
+++ Controllers/jobController.js
@@ -1,33 +1,55 @@
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const app =   express();
+app.use(fileUpload());
 const jobModel = require("../Models/jobModel");
+
+
 
 const applyJobPage = (req, res) => {
   res.end("applyJob render()");
 };
-const applyJob = async (req, res) => {
-  const { userName, qualification, email, cnic, phoneNumber, cv, age } = req.body;
-  console.log(req.body)
-  try {
-     if (!userName || !qualification || !email || !cnic || !phoneNumber || !cv || !age) {
-        return res.status(400).json({ message: "All fields are required!" });
-     }
 
-     const newApplicant = await jobModel.create({
+
+const applyJob = async (req, res) => {
+  const { userName, qualification, email, cnic, phoneNumber, age } = req.body;
+
+  try {
+    if (!userName || !qualification || !email || !cnic || !phoneNumber || !age) {
+      return res.status(400).json({ message: "All fields are required!" });
+    }
+
+    if (!req.files || Object.keys(req.files).length === 0) {
+      return res.status(400).json({ message: 'No files were uploaded.' });
+    }
+
+    const cvFile = req.files.cv;
+    // path for saving the resume
+    const cvPath = `${__dirname}/../uploads/${cvFile.name}`;
+
+    cvFile.mv(cvPath, (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ message: 'File upload failed.' });
+      }
+
+      const newApplicant = jobModel.create({
         userName,
         qualification,
         email,
         cnic,
         phoneNumber,
-        cv,
+        cv: cvPath,
         age,
-     });
+      });
 
-     console.log("applicant created: ", newApplicant);
-     return res.status(200).json({ message: "new applicant created" });
+      console.log("applicant created: ", newApplicant);
+      return res.status(200).json({ message: "New applicant created" });
+    });
   } catch (error) {
-     console.error(error);
-     return res.status(500).json({ message: "Internal Server Error" });
+    console.error(error);
+    return res.status(500).json({ message: "Internal Server Error" });
   }
 };
-
 
 module.exports = { applyJobPage, applyJob };
