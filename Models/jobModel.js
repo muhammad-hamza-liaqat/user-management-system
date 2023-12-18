f809cfe905c67f2dd6b23c3fb998a168b0cd58f4
Muhammad Hamza <mh408800@gmail.com>
2023-12-18 12:35:59 +0500

    jobModel.js

---
+++ Models/jobModel.js
@@ -0,0 +1,69 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../Database/connection');
+const jobModel = sequelize.define('jobs', {
+    applicantId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        primaryKey: true,
+    },
+    userName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true,
+        },
+    },
+    qualification: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    cnic: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    address: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    phoneNumber: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    status: {
+        type: DataTypes.ENUM('pending', 'accepted', 'rejected'),
+        allowNull: false,
+        defaultValue: 'pending'
+    },
+    cv: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    age: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    isDelete: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+    }
+}, {
+    tableName: 'applicants',
+    timestamps: true,
+});
+
+sequelize.sync()
+    .then(() => {
+        console.log('jobModel synchronized with the database(finalProject).');
+    })
+    .catch((error) => {
+        console.error('Error synchronizing jobModel', error);
+});
+
+module.exports = jobModel;
