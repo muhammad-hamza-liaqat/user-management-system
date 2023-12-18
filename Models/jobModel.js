459b59e99da42d82262ed1892a453b0bfe061312
Muhammad Hamza <mh408800@gmail.com>
2023-12-18 12:04:13 +0500

    jobModel modify

--- Models/jobModel.js
+++ Models/jobModel.js
@@ -1,61 +1,71 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../Database/connection");
 
-const jobModel = sequelize.define("jobs", {
-  applicantID: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const jobModel = sequelize.define(
+  "jobs",
+  {
+    applicantID: {
+      type: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
+    userName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    qualification: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    cnic: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    phoneNumber: {
+      type: DataTypes.STRING, 
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.ENUM("accepted", "rejected", "pending"),
+      allowNull: true,
+      defaultValue: "pending",
+    },
+    cv: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    age: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    isDelete: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true,
+      defaultValue: false,
+    },
   },
-  userName: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  qualification: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  cnic: {
-    type: DataTypes.STRING, // Corrected data type
-    allowNull: false,
-  },
-  phoneNumber: {
-    type: DataTypes.STRING, // Corrected data type
-    allowNull: false,
-  },
-  status: {
-    type: DataTypes.ENUM("accepted", "rejected", "pending"),
-    allowNull: true,
-    defaultValue: "pending",
-  },
-  cv: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  age: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  isDelete: {
-    type: DataTypes.BOOLEAN,
-    allowNull: true,
-    defaultValue: false
-  },
-}, {
-  tableName: "jobs",
-  timestamps: true
-});
+  {
+    tableName: "jobs",
+    timestamps: true,
+  }
+);
 
-sequelize.sync()
+sequelize
+  .sync()
   .then(() => {
-    console.log('jobModel synchronized with the database(finalProject).');
+    console.log("jobModel synchronized with the database(finalProject).");
   })
   .catch((error) => {
-    console.error('Error synchronizing jobModel', error);
+    console.error("Error synchronizing jobModel", error);
   });
 
 module.exports = jobModel;
