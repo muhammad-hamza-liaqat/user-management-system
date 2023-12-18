02dbc878a231432444412eb0732bd0c203c73a7d
Muhammad Hamza <mh408800@gmail.com>
2023-12-18 12:53:45 +0500

    .

--- Controllers/jobController.js
+++ Controllers/jobController.js
@@ -2,6 +2,7 @@ const multer = require("multer");
 const { v4: uuidv4 } = require("uuid");
 const jobModel = require("../Models/jobModel");
 
+// multer configturation for uploading the files into the uploads folder.
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "uploads/");
