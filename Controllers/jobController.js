76aa7a26e52c8906bb65e243e6086d299ee5ff68
Muhammad Hamza <mh408800@gmail.com>
2023-12-18 15:58:54 +0500

    download cv added

--- Controllers/jobController.js
+++ Controllers/jobController.js
@@ -1,32 +1,33 @@
-const express = require('express');
-const multer = require('multer');
-const { v4: uuidv4 } = require('uuid');
-const jobModel = require('../Models/jobModel');
-const fs = require('fs');
+const express = require("express");
+const multer = require("multer");
+const { v4: uuidv4 } = require("uuid");
+const jobModel = require("../Models/jobModel");
+const fs = require("fs");
+const path = require("path");
 
 // Define storage configuration
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     // Specify the destination folder where the uploaded files will be stored
-    cb(null, 'uploads/');
+    cb(null, "uploads/");
   },
   filename: function (req, file, cb) {
     // Customize the filename to include the email address with PDF extension
-    const email = req.body.email || 'default'; // Use a default value if email is not available
+    const email = req.body.email || "default"; // Use a default value if email is not available
     const fileName = `${email}.pdf`;
     cb(null, fileName);
   },
 });
 
 // Create the multer middleware with the defined storage
-const upload = multer({ storage: storage }).single('cv');
+const upload = multer({ storage: storage }).single("cv");
 
 const handleFileUpload = (req, res, next) => {
   upload(req, res, function (err) {
     if (err instanceof multer.MulterError) {
-      return res.status(400).json({ error: 'File upload error' });
+      return res.status(400).json({ error: "File upload error" });
     } else if (err) {
-      return res.status(500).json({ error: 'Internal server error' });
+      return res.status(500).json({ error: "Internal server error" });
     }
     next();
   });
@@ -45,7 +46,7 @@ const submitForm = async (req, res) => {
     isDelete,
   } = req.body;
 
-  let cvPath = '';
+  let cvPath = "";
   if (req.file) {
     cvPath = req.file.path;
   }
@@ -67,21 +68,21 @@ const submitForm = async (req, res) => {
 
     return res
       .status(201)
-      .json({ message: 'Applicant created successfully', data: newApplicant });
+      .json({ message: "Applicant created successfully", data: newApplicant });
   } catch (error) {
     if (
-      error.name === 'SequelizeValidationError' ||
-      error.name === 'SequelizeUniqueConstraintError'
+      error.name === "SequelizeValidationError" ||
+      error.name === "SequelizeUniqueConstraintError"
     ) {
       const errorMessage =
         error.errors && error.errors.length > 0
           ? error.errors[0].message
-          : 'Validation error';
+          : "Validation error";
       return res.status(400).json({ error: errorMessage });
     }
 
-    console.error('Error creating applicant:', error);
-    return res.status(500).json({ error: 'Internal server error' });
+    console.error("Error creating applicant:", error);
+    return res.status(500).json({ error: "Internal server error" });
   }
 };
 
@@ -89,8 +90,47 @@ const applyJob = (req, res) => {
   res.end("hello from the applyJob controller");
 };
 
+const downloadResume = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const applicant = await jobModel.findOne({
+      where: {
+        applicantId: id,
+      },
+    });
+    if (!applicant) {
+      return res.status(404).send("CV not found");
+    }
+    const cvFilePath = applicant.cv;
+    console.log(cvFilePath)
+    console.log("CV File Path:", cvFilePath);
+    if (fs.existsSync(cvFilePath)) {
+      const fileExtension = path.extname(applicant.cv);
+
+      const applicantEmail = applicant.email;
+
+      const sanitizedEmail = applicantEmail.replace(/[@.]/g, "_");
+
+      res.setHeader(
+        "Content-Disposition",
+        `attachment; filename="${sanitizedEmail}_CV${fileExtension}"`
+      );
+      res.setHeader("Content-Type", "application/pdf");
+
+      const fileStream = fs.createReadStream(cvFilePath);
+      fileStream.pipe(res);
+    } else {
+      res.status(404).send("CV file not found");
+    }
+  } catch (error) {
+    console.error("Error downloading CV:", error);
+    res.status(500).send("Internal server error");
+  }
+};
+
 module.exports = {
   handleFileUpload,
   submitForm,
-  applyJob
+  applyJob,
+  downloadResume,
 };
--- Routes/jobRoute.js
+++ Routes/jobRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { handleFileUpload, submitForm ,applyJob } = require("../Controllers/jobController");
+const { handleFileUpload, submitForm ,applyJob, downloadResume } = require("../Controllers/jobController");
 
 const jobRouter = express.Router();
 
 jobRouter.route("/apply-job").get(applyJob).post(handleFileUpload, submitForm);
+jobRouter.route("/download-cv/:id").get(downloadResume)
 
 module.exports = jobRouter;
