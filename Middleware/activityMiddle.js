6c5aec7dc275128837929f33aabf14db2d5666bb
Muhammad Hamza <mh408800@gmail.com>
2023-12-22 13:04:47 +0500

    .

--- Middleware/activityMiddle.js
+++ Middleware/activityMiddle.js
@@ -28,7 +28,7 @@ const logUserActivity = async (req, res, next) => {
           action: "User Login",
           username: userName,
           userEmail: email,
-          details: `User "${email}" jsut logged in`,
+          details: `User "${email}" just logged in`,
         };
         console.log("log recorded for :8080/api/user/login-user");
       } else {
@@ -51,32 +51,41 @@ const logUserActivity = async (req, res, next) => {
       } else {
         console.log(`User with email ${email} not found`);
       }
-    } else if (
-      req.path.startsWith("/set-password/") &&
-      req.method === "PATCH"
-    ) {
+    } else if (req.path.startsWith("/set-password/") && req.method === "PATCH") {
       // For set password
       const { token } = req.params;
-      logData = {
-        action: "Set Password",
-        details: `Password reset completed with token ${token}`,
-      };
-      console.log("log recorded for :8080/api/user/set-password");
-    } else if (
-      req.path.startsWith("/change-password") &&
-      req.method === "POST"
-    ) {
-      // For set password
-      const { email } = req.body;
-      logData = {
-        action: "create-password",
-        details: `Password changed completed with token ${email}`,
-      };
-      console.log("log recorded for :8080/api/user/set-password");
+      const user = await User.findOne({ where: { rememberToken: token } });
+      if (user) {
+        const { firstName, lastName } = user;
+        const userName = `${firstName} ${lastName}`;
+        logData = {
+          action: "Set Password",
+          username: userName,
+          userEmail: user.email,
+          details: `Password reset completed with token ${token}`,
+        };
+        console.log("log recorded for :8080/api/user/set-password");
+      }
+    } else if (req.path.startsWith("/change-password") && req.method === "POST") {
+      // For change password
+      const email = req.user.email;
+      const user = await User.findOne({ where: { email } });
+      if (user) {
+        const { firstName, lastName } = user;
+        const userName = `${firstName} ${lastName}`;
+        logData = {
+          action: "Change Password",
+          username: userName,
+          userEmail: email,
+          details: `Password Change completed with email ${email}`,
+        };
+        console.log("log recorded for :8080/api/user/change-password");
+      }
     } else {
       // If the route doesn't match any specific path or method, proceed to next middleware
       return next();
     }
+
     res.on("finish", async () => {
       try {
         logData.statusCode = res.statusCode;
@@ -93,6 +102,7 @@ const logUserActivity = async (req, res, next) => {
         console.error("Error creating log:", error);
       }
     });
+    
     next();
   } catch (error) {
     console.error("Error logging user activity:", error);
